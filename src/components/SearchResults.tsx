416f6fa3cae5a1ee8c802a1c111061312065104f
Adlet Baiteliyev <bah_92adlet@mail.ru>
2023-05-26 14:06:35 +0600

    search result without url

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -2,12 +2,15 @@ import "../pages/SearchResults.css"
 
 import React, { useEffect, useState } from "react"
 import axios from "axios"
-import { useLocation } from "react-router-dom"
+import { Link, useLocation, useSearchParams } from "react-router-dom"
+import SortIcon from "../assets/sortIcon.svg";
 
 const SearchResults = ({ query }) => {
   const location = useLocation()
   const queryParams = new URLSearchParams(location.search)
   const searchQuery = queryParams.get("query")
+  // const [searchParams, setSearchParams] = useSearchParams()
+  // const [queryParams, setQueryParams] = useState(searchParams.get("query"))
   const [results, setResults] = useState([])
 
   useEffect(() => {
@@ -18,9 +21,8 @@ const SearchResults = ({ query }) => {
         // const response = await axios.get(
         //   `https://rest.uniprot.org/uniprotkb/search?fields=accession,reviewed,id,protein_name,gene_names,organism_name,length,ft_peptide,cc_subcellular_location&query=${searchQuery}`,
         // )
-        const apiUrl = `https://rest.uniprot.org/uniprotkb/search?fields=accession,reviewed,id,protein_name,gene_names,organism_name,length,ft_peptide,cc_subcellular_location&query=${searchQuery}`;
+        const apiUrl = `https://rest.uniprot.org/uniprotkb/search?fields=accession,reviewed,id,protein_name,gene_names,organism_name,length,ft_peptide,cc_subcellular_location&query=${query}`;
         const response = await axios.get(apiUrl);
-        console.log(response)
 
         setResults(response.data.results)
       } catch (error) {
@@ -41,38 +43,58 @@ const SearchResults = ({ query }) => {
   }
 
   return (
-    <div className="table-container">
-      <table>
-        <tr>
-          <th>{"#"}</th>
-          <th>{"Entry"}</th>
-          <th>{"Entry names"}</th>
-          <th>{"Genes "}</th>
-          <th>{"Organism"}</th>
-          <th>{"Subcellular Location"}</th>
-          <th>{"Length"}</th>
-        </tr>
-        {/* <tbody> */}
-        {results.map((item, index) => (
-          <tr key={index}>
-            <td>{index + 1}</td>
-            <td>{item.primaryAccession}</td>
-            <td>{item.uniProtkbId}</td>
-            <td>
-              {" "}
-              {item.genes ? item.genes[0]?.geneName?.value : "no genes data "}
-            </td>
-            <td>{item.organism.scientificName}</td>
-            <td>
-              {item.comments
-                ? item.comments[0]?.subcellularLocations[0]?.location.value
-                : "no location data"}
-            </td>
-            <td>{item.sequence.length}</td>
+    <div>
+      <div className="results-number">
+        <p>{`${results.length} Search results found for "${query}" `}</p>
+    </div>
+      <div className="table-container">
+        <table>
+          <tr>
+            <th className="number">{"#"}</th>
+            <th className="entry">
+              {"Entry"}<img src={SortIcon}/>
+              </th>
+            <th className="entry-name">
+              {"Entry names"}<img src={SortIcon}/>
+              </th>
+            <th className="genes">
+              {"Genes "}<img src={SortIcon}/>
+              </th>
+            <th className="organism">
+              {"Organism"}<img src={SortIcon}/>
+              </th>
+            <th className="location">
+              {"Subcellular Location"}
+              </th>
+            <th className="length">
+              {"Length"}<img src={SortIcon}/>
+              </th>
           </tr>
-        ))}
-        {/* </tbody> */}
-      </table>
+          {/* <tbody> */}
+          {results.map((item, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td className="primaryAccession"><Link to={`/protein/${item.primaryAccession}`}>
+                    {item.primaryAccession}
+                  </Link>
+              </td>
+              <td className="uniProtkbId">{item.uniProtkbId}</td>
+              <td className="genes-column">
+                {" "}
+                {item.genes ? item.genes[0]?.geneName?.value : "no genes data "}
+              </td>
+              <td className="scientificName">{item.organism.scientificName}</td>
+              <td className="subcellularLocations">
+                {item.comments
+                  ? item.comments[0]?.subcellularLocations[0]?.location.value
+                  : "no location data"}
+              </td>
+              <td className="lengthLast">{item.sequence.length}</td>
+            </tr>
+          ))}
+          {/* </tbody> */}
+        </table>
+      </div>
     </div>
   )
 }
